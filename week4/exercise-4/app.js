cd632b1d93e2a6dc203ce2ed6418b466484b0032
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2018-10-02 22:18:35 -0400

    COMP3123 - Week 4 - Lab - Exercise 4 Updated

--- week4/exercise-4/app.js
+++ week4/exercise-4/app.js
@@ -5,36 +5,29 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
   if (req.url == '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify('No data found'));
   }
   
   else if (req.url == '/api/favoritebook') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
     let favoritebook = store.booksModule.favoriteBook();
     res.end(JSON.stringify(`Favorite Book: ${favoritebook.title} - Author: ${favoritebook.author}`));
   }
 
   else if (req.url == '/api/getbooks') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
     let books = store.booksModule.getBookRecommendations();
     res.end(JSON.stringify(books));
   }
 
   else if (req.url == '/api/getvideos') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
     let videos = store.videosModule.getVideoRecommendations();
     res.end(JSON.stringify(videos));
   }
 
   else {
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify('Error'));
   }
 });
